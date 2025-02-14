c80bf65d40fd53da49ca645a145c471477672115
vijay-kittu <vijaykittu16@gmail.com>
2025-02-14 19:15:47 +0530

    made changes to backend code, added new dependencies

---
+++ FoodOrderWebsite/src/main/java/com/food_order/FoodOrderWebsite/FoodOrderWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.food_order.FoodOrderWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderWebsiteApplication.class, args);
+	}
+
+}
---
+++ FoodOrderWebsite/src/main/java/com/food_order/FoodOrderWebsite/entity/User.java
@@ -0,0 +1,4 @@
+package com.food_order.FoodOrderWebsite.entity;
+
+public class User {
+}
