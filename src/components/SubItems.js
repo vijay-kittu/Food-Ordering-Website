288db1f167cb36b3eac6b7e6cc032b6e07c8943d
vijay-kittu <vijaykittu16@gmail.com>
2025-02-19 23:54:19 +0530

    changes in App.css

--- src/components/SubItems.js
+++ src/components/SubItems.js
@@ -273,10 +273,13 @@ export const SubItems = ({ selectedItem }) => {
             <div>Cart</div>
             {cart.map((item, index) => (
                 <div key={index} className="cart-item">
-                <h4>{item.itemName}</h4>
-                <p>Price: ₹{item.price}</p>
-                <p>Quantity: {item.quantity}</p>
-                <p className="total-price">Total Price: {item.price * item.quantity}</p>
+                    <div className="cart-subitem">
+                        <h4>{item.itemName}</h4>
+                        <p className="right-align">Price: ₹{item.price}</p>
+                        <p>Quantity: {item.quantity}</p>
+                    </div>
+                    
+                    <p className="right-align">Total Price: {item.price * item.quantity}</p>
                 </div>
             ))}
         
