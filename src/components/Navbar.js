4795be0613ce19a01a00ae894f85775fd485cf39
vijay-kittu <vijaykittu16@gmail.com>
2025-03-07 23:50:10 +0530

    changes in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 export const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <div >
-                <Link className="homepage"  to="/"> 𝓑𝓪𝓮 𝓢𝓾𝔃𝔂 </Link>
-            </div>
-            <div className="right">
-                {/*<Link className="links " to="/cart"> Cart </Link>*/}
-                <Link style={{marginRight:'10px'}} className="links " to="/login"> My Profile </Link>
-            </div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="navbar">
+      <div>
+        <Link className="homepage" to="/">
+          {" "}
+          𝓑𝓪𝓮 𝓢𝓾𝔃𝔂{" "}
+        </Link>
+      </div>
+      <div className="right">
+        <Link className="links " to="/cart">
+          {" "}
+          Cart{" "}
+        </Link>
+        <Link style={{ marginRight: "10px" }} className="links " to="/login">
+          {" "}
+          My Profile{" "}
+        </Link>
+      </div>
+    </div>
+  );
+};
